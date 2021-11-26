4365638153fbad2b45dea5ff87a0af45301d677f
Fausto Siqueira <fausto.siqueira@gmail.com>
2021-11-26 20:05:30 +0100

    SEO checks test case refactor, meta tags checks and usage of Fixtures

--- cypress/integration/seo-checks-spec.js
+++ cypress/integration/seo-checks-spec.js
@@ -1,56 +1,76 @@
 describe('Apuestas Online SEO', () => {
 
-  beforeEach( () => {
-    cy.visit('https://apuestasonline.net/')
-    cy.acceptLegalAgePopUp()
+  before(function () {
+    cy.fixture('apuestas-online').then((home) => {
+      this.home = home
+    })
   })
 
-  it('There is only on H1 element', function () {
+  beforeEach( () => {
+    cy.visit('https://apuestasonline.net/')
+    // cy.acceptLegalAgePopUp()
+  })
+
+  it('has only one H1 element', () => {
     cy.get('h1').should('have.length', 1)
   })
 
-  it('There are at least two H2 elements', function () {
+  it('has at least two H2 elements', () => {
     cy.get('h2').its('length').should('be.gte', 2)
   })
 
-  it('All images have alt tags', function () {
+  it('has alt tags on all images', () => {
     cy.get('img').should('have.attr', 'alt')
   })
 
-  it('All images have alt tags', function () {
-    cy.get('img').should('have.attr', 'alt')
-  })
-
-  //WIP @failing
-  it.skip('Site has only one heading with with a title and meta description', function () {
+  it.skip('has only one heading with with a title and meta description', function () {
     cy.get('head')
-    .should('have.length', 1)
+      .should('have.length', 1)
     
     cy.get('head title')
-    .should('have.length', 1)
+      .should('have.length', 1)
+      .and('have.text', this.home.title)
     
-    cy.get('head meta')
-    .should('have.attr', 'name')
-    .and('equal', 'description')
+    cy.get('head')
+      .get('meta[name="description"]')
+      .should('have.prop', 'content')
+      .and('equal', this.home.metaDescriptionText)
   })
 
-  it.skip('Site content is divided by multiple subheadings', function () {
-    // Work in progress
+  it.skip('has a viewport meta tag', function () {
+    cy.get('head')
+      .get('meta[name="viewport"]')
+      .should('have.prop', 'content')
+      .and('equal', this.home.viewport)
   })
 
-  it('Site has all canconicals valid', function () {
+  it('has content divided by multiple subheadings', () => {
+    cy.get('.content').get('h2')
+      .should('have.length', 7)
+
+    cy.get('.content').get('h3')
+      .should('have.length', 24)
+
+    cy.get('.content').get('h4')
+      .should('have.length', 6)
+
+    cy.get('.content').get('h5')
+      .should('have.length', 0)
+  })
+
+  it(' has all the canconicals valid', () => {
     cy.get('link')
-    .should('have.attr', 'rel')
-    .and('equal', 'canonical')
+      .should('have.attr', 'rel')
+      .and('equal', 'canonical')
     
     cy.get('link')
-    .should('have.attr', 'href')
-    .and('not.be.empty') 
+      .should('have.attr', 'href')
+      .and('not.be.empty')
   })
 
-  it('Site has an HTML lang set', () => {
+  it('has a HTML lang set', () => {
     cy.get('html')
-    .should('have.attr', 'lang')
-    .and('not.be.empty')
+      .should('have.attr', 'lang')
+      .and('not.be.empty')
   })
 })
