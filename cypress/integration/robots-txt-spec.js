46ece7690c732ead69c254f42dfa462342be88ec
Fausto Siqueira <fausto.siqueira@gmail.com>
2021-11-27 19:56:36 +0100

    Adding sitemap validation

--- cypress/integration/robots-txt-spec.js
+++ cypress/integration/robots-txt-spec.js
@@ -1,21 +1,27 @@
-describe('Site has robots.txt target', () => {
+describe('robots.txt related functionality', () => {
 
-    before(function () {
-      cy.fixture('apuestas-online.json').then((home) => {
-        this.home = home
-      })
+  before(function () {
+    cy.fixture('apuestas-online.json').then((home) => {
+      this.home = home
     })
+  })
 
-    it('has a robots meta tag', function () {
-      cy.visit('https://apuestasonline.net/')
-      cy.get('head')
-        .get('meta[name="robots"]')
-        .should('have.prop', 'content')
-        .and('equal', this.home.metaRobots)
-    })
+  it('has a robots meta tag', function () {
+    cy.visit('https://apuestasonline.net/')
 
-    it('Contains a robots.txt', () => {
-      cy.request('https://apuestasonline.net/robots.txt')
-          .its('body').should('include', 'Sitemap: https://apuestasonline.net/sitemap_index.xml')
+    cy.get('head')
+      .get('meta[name="robots"]')
+      .should('have.prop', 'content')
+      .and('equal', this.home.metaRobots)
+  })
+
+  it('Contains a robots.txt file', () => {
+    cy.request('https://apuestasonline.net/robots.txt')
+      .its('body').should('include', 'Sitemap: https://apuestasonline.net/sitemap_index.xml')
+  })
+
+  it('Contains a sitemap', () => {
+    cy.request('https://apuestasonline.net/sitemap_index.xml')
+      .its('body').should('include', 'XML Sitemap')
   })
 })
