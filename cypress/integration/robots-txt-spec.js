761446ffb4318167728359db94daf232294eb6a9
Fausto Siqueira <fausto.siqueira@gmail.com>
2021-11-27 19:44:06 +0100

    Adding robot meta tag validation

--- cypress/integration/robots-txt-spec.js
+++ cypress/integration/robots-txt-spec.js
@@ -1,11 +1,21 @@
 describe('Site has robots.txt target', () => {
 
-    beforeEach( () => {
+    before(function () {
+      cy.fixture('apuestas-online.json').then((home) => {
+        this.home = home
+      })
+    })
+
+    it('has a robots meta tag', function () {
       cy.visit('https://apuestasonline.net/')
+      cy.get('head')
+        .get('meta[name="robots"]')
+        .should('have.prop', 'content')
+        .and('equal', this.home.metaRobots)
     })
-  
-    it('Contains a robots.txt', function () {
-        cy.request('https://apuestasonline.net/robots.txt')
-            .its('body').should('include', 'Sitemap: https://apuestasonline.net/sitemap_index.xml')
-    })
+
+    it('Contains a robots.txt', () => {
+      cy.request('https://apuestasonline.net/robots.txt')
+          .its('body').should('include', 'Sitemap: https://apuestasonline.net/sitemap_index.xml')
+  })
 })
