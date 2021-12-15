9914c980558f60a51534120e1815ba79d06f926b
Fausto Siqueira <fausto.siqueira@gmail.com>
2021-12-15 21:42:50 +0100

    SEO checklist for Images

--- cypress/integration/images/images-spec.js
+++ cypress/integration/images/images-spec.js
@@ -4,7 +4,7 @@ describe('Images SEO checklist', () => {
         cy.visit('https://apuestasonline.net/')
       })
 
-    it('has alt tags on all images and defined width and height', () => {
+    it.skip('has alt tags on all images and defined width and height', () => {
         cy.get('img').then(($img) => {
           expect($img).to.have.attr('alt').not.to.be.empty
           expect($img).to.have.attr('width')
@@ -12,11 +12,23 @@ describe('Images SEO checklist', () => {
         })
     })
 
-    it('responds 200 OK for all image requests', () => {
+    it.skip('responds 200 OK for all image requests', () => {
         cy.get('img')
             .each(img => {
-                cy.log(img.attr('src'))
                 cy.request(img.attr('src'))
         })
+    })
+
+    it('all PNG image sizes are less than 150kb', () => {
+        cy.get('img')
+            .each(img => {
+                if ( img.attr('src').includes('.png') )
+                    cy.request(img.attr('src'))
+                        .its('headers')
+                        .its('content-length')
+                        .then((contentLength) => {                            
+                            expect(parseInt(contentLength)).to.be.lessThan(150000)
+                        })
+            })
     })
 })
