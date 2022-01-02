2c4cb316c67104bdf0585fa2cad184397f6363a3
Fausto Siqueira <fausto@kafe.rocks>
2022-01-02 18:14:01 +0100

    Refactor for baseUrl

--- cypress/integration/html-validation/html-specs.js
+++ cypress/integration/html-validation/html-specs.js
@@ -1,7 +1,7 @@
 describe('HMTL Specs', () => {
 
     it.skip("HTML5 should be valid", () => {
-        cy.visit("https://apuestasonline.net/");
+        cy.visit('');
         cy.htmlvalidate();
     })
 })
